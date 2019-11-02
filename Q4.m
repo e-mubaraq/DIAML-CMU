2500d1f332e50d51b837d674db91cad4e5704b06
Mubarak <mikail.mubarak1@gmail.com>
2019-11-02 15:31:47 +0200

    Changes to the Q4 code

--- Q4.m
+++ Q4.m
@@ -7,6 +7,8 @@
 % survived = titanic.survived;
 
 n = length(name);
+label = {'0-10', '11-20', '21-30', '31-40', '41-50', '51-60', '61-70', '71-80'};
+age_group = ordinal(age, label, [], [0, 11, 21, 31, 41, 51, 61, 71, 80]);
 
 num_of_survived = length(find(survived == 1));
 prob_of_survival = num_of_survived / n
